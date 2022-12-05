7a184f24eaa01d5fcceb140df3206d42f2fc23c1
Jarl Jakobsson <jarl.jakobsson@gmail.com>
2022-12-05 23:18:22 +0100

    added better roll system

--- Characters.py
+++ Characters.py
@@ -3,25 +3,53 @@ import random
 
 class Character:
     def __init__(self) -> None:
-        is_alive = self.is_alive
+        self.is_alive = True
         self.initative = 0
-        self.endurance = 0
+        self.health = 0
         self.attack = 0
         self.agility = 0
+        self.name = ""
+        self.max_health = 0
 
-    def attack_roll(attack_stat):
-        attack_value = random.randint(0, 6) * attack_stat
-        print(f"i try attack ({attack_value} attack roll")
-        return attack_value
+    def roll_die(self):
+        return random.randint(1, 6)
+
+    def roll(self, value):
+        sum = 0
+        for i in range(value):
+            sum += self.roll_die()
+        print(f"{self.name}I try something! (Rolled {sum})")
+        return sum
+
+    def attack_roll(self):
+        attack = 0
+        for i in range(self.attack):
+            attack += self.roll_die()
+        print(f"{self.name}I try attack! ({attack} attack roll")
+        return attack
+
+    def initative_roll(self):
+        initative = 0
+        for i in range(self.initative):
+            initative += self.roll_die()
+        print(f"{self.name}I try initiate! ({initative} initative roll")
+        return initative
 
     def death(self):
         self.is_alive = False
         print("im death gg")
 
-    def dodge_roll(self, dodge_stat):
-        dodge_value = random.randint(0, 6) * dodge_stat
-        print(f"i try dodge ({dodge_value} dodge roll)")
-        return dodge_value
+    def dodge_roll(self):
+        dodge = 0
+        for i in range(self.agility):
+            dodge += self.roll_die()
+        print(f"{self.name}I try dodge! ({dodge} dodge roll")
+        return dodge
 
     def take_dmg(self):
-        self.endurance -= 1
+        self.health -= 1
+        if self.health == 0:
+            self.death()
+
+    def heal(self):
+        self.health = self.max_health
