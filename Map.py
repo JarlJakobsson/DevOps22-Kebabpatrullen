2e4dff42d5db97b522cef4457d9b1b1754f3669f
Jarl Jakobsson <jarl.jakobsson@gmail.com>
2022-12-07 21:41:43 +0100

    Added code for object room creation

--- Map.py
+++ Map.py
@@ -1,16 +1,45 @@
+from Room import Room
+
+
+## There probably is a better way but, I create a map template with "X"s and then replace all the "X"s with room objects
+## and give them the correct room_index
 class Map:
-    def __init__(self, size = 4):
+    def __init__(self, size=4, player_position=(0, 0)):
         self.map = [["X"] * size for i in range(size)]
+        for row in range(size):
+            for room in range(size):
+                self.map[row][room] = Room((row, room))
+
         self.is_start = True
+        self.size = size
+        self.player_position = player_position
 
-    # def create_map(self, size=4):
-    #     self.map = [["X"] * size for i in range(size)]
+    def mark_player_position(self, position):
+        x, y = position
+        self.map[x][y].name = "P"
 
-    def mark_player_position(self, pos1, pos2):
-        self.map[pos1][pos2] = "P"
+    def mark_visited_room(self, position):
+        x, y = position
+        self.map[x][y].name = "0"
 
-    def mark_visited_room(self, pos1, pos2):
-        self.map[pos1][pos2] = "0"
+    def mark_player_leave_room(self):
+        for row in range(self.size):
+            for room in range(self.size):
+                if self.map[row][room].name == "P":
+                    self.map[row][room].name = "0"
 
-    def give_player_position(self, current):
-        pass
+    def print_map(self):
+        for row in self.map:
+            print(row)
+        print("\n -------------------\n")
+
+    def move_player(self, position):
+        x, y = position
+
+
+# x = Map()
+# # x.print_map()
+# move = (0,0)
+# x.mark_player_position(move)
+# x.print_map()
+# x.mark_visited_room(0,0)
