420eebb0bc4656ba1c1c369601f8d6d2f98cc6d7
Jarl Jakobsson <jarl.jakobsson@gmail.com>
2022-12-07 21:36:53 +0100

    started working on moveing character

--- Game.py
+++ Game.py
@@ -1,5 +1,8 @@
 from PlayerRoles import Knight, Wizard, Thief
 from Map import Map
+import Room
+import random
+import Enemies
 
 
 class Game:
@@ -9,6 +12,11 @@ class Game:
     def create_map(self):
         self.map = Map(int(input("Enter size: ")))
 
+    def move(self, position):
+        move = 1
+        self.player.position = position
+        self.map.player_position = position
+
     def create_player(self, position, role):
         if role == 1:
             self.player = Knight(position)
@@ -17,13 +25,25 @@ class Game:
         elif role == 3:
             self.player = Thief(position)
 
-    def main(self):
-        val = mainmeny()
-        valcheck(val) # få in namn och rol och position
-        self.create_player(val)
-        nytt val size
-        sizecheck(size) #size and start
-        self.create_map(size and start)
-        self.map.mark_player_position(self.player.position)
-        self.player.move(input("where do you want to move? "))
+    def check_room(self, position):
+        x, y = position
+        if self.map.map[x][y].monster:
+            print("Monster in here")
+            self.battle(position)
 
+    def battle(self, position):
+        x, y = position
+        if self.player.initative_roll() >= self.map.map[x][y].monster.initative_roll():
+            print("player initative")
+        else:
+            print("monster initatitve")
+
+    def main(self):
+        self.player = Wizard()
+        self.map = Map()
+        self.map.mark_player_position(self.player.position)
+        self.check_room(self.player.position)
+
+
+x = Game()
+x.main()
