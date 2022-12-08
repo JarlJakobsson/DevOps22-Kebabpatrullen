e3534e67874375cf9f060b3e482f5b0c8537fdc4
Jarl Jakobsson <jarl.jakobsson@gmail.com>
2022-12-08 14:07:50 +0100

    Added move function + more

--- Game.py
+++ Game.py
@@ -1,49 +1,91 @@
 from PlayerRoles import Knight, Wizard, Thief
 from Map import Map
-import Room
-import random
-import Mosters
+from MoveMenu import Move_menu
+from StartMenu import Start_menu
 
 
 class Game:
     def __init__(self):
-        pass
+        self.start_menu = Start_menu()
+        self.move_menu = Move_menu()
+        self.initiatior = 0
+
+    def wait_input(self):
+        input("\nPress any key...\n")
 
     def create_map(self):
         self.map = Map(int(input("Enter size: ")))
 
-    def move(self, position):
-        move = 1
-        self.player.position = position
-        self.map.player_position = position
-
-    def create_player(self, position, role):
+    def create_player(self, role):
         if role == 1:
-            self.player = Knight(position)
+            self.player = Knight()
         elif role == 2:
-            self.player = Wizard(position)
+            self.player = Wizard()
         elif role == 3:
-            self.player = Thief(position)
+            self.player = Thief()
 
     def check_room(self, position):
+        # Splits position into x and y
         x, y = position
+        # Check if self.map.map[x][y].monster is True. If True prints the monster name and battle menu
         if self.map.map[x][y].monster:
-            print("Monster in here")
+            print(f"{self.player.name}: There is a {self.map.map[x][y].monster.name}")
             self.battle(position)
+        # If false prints that no monsters in room
+        else:
+            print(f"\n{self.player.name}: ...No monsters in here...\n")
+        # After battle, Player loots treasure, monster get removed from room, treasure get removed from room
+        self.player.get_treasure(self.map.map[x][y].treasure)
+        self.map.map[x][y].monster = 0
+        self.map.map[x][y].treasure = 0
+        self.wait_input()
 
     def battle(self, position):
-        x, y = position
-        if self.player.initative_roll() >= self.map.map[x][y].monster.initative_roll():
-            print("player initative")
+        # Compares player initiative roll against monster initiative roll and sets the winner as initiator
+        if (
+            self.player.initative_roll()
+            >= self.map.map[position[0]][position[1]].monster.initative_roll()
+        ):
+            self.initiatior = 1
+            print("[GAME] Player takes initative")
         else:
-            print("monster initatitve")
+            print("[GAME] Monster initatitve")
+        self.wait_input()
+
+    def move_player(self, direction):
+        # Splits direction and player positon
+        x, y = direction
+        a, b = self.map.player_position
+        a += x
+        b += y
+        # Checks if new room exists
+        if a == -1 or a > self.map.size - 1 or b == -1 or b > self.map.size - 1:
+            print(f"{self.player.name}: Ouch... There is a wall there...")
+            self.wait_input()
+            return False
+        # If exists, Marks current room as visited, Updates player location, Marks new room as currently in
+        else:
+            self.map.mark_visited_room(self.map.player_position)
+            print(f"*** {a,b} ***")
+            self.map.player_position = (a, b)
+            self.map.mark_player_position((a, b))
+            return True
 
     def main(self):
-        self.player = Wizard()
+        self.start_menu.run_menu()
+        print(self.start_menu.role)
+        self.create_player(self.start_menu.role)
+        self.player.name = self.start_menu.name
         self.map = Map()
-        self.map.mark_player_position(self.player.position)
-        self.check_room(self.player.position)
+        self.map.mark_player_position(self.map.player_position)
+        while True:
+            self.move_player(self.map.player_position)
+            while True:
+                self.map.print_map()
+                self.move_menu.run_menu()
+                if self.check_room_exist(self.move_menu.direction):
+                    break
 
 
-x = Game()
-x.main()
+game = Game()
+game.main()
