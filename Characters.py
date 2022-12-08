21ab21d77f80deda288247203eeeacdebc4f1db2
Jarl Jakobsson <jarl.jakobsson@gmail.com>
2022-12-08 14:05:01 +0100

    Added Comments

--- Characters.py
+++ Characters.py
@@ -2,6 +2,7 @@ import random
 
 
 class Character:
+    # Initiates character attributes
     def __init__(self) -> None:
         self.is_alive = True
         self.initiative = 0
@@ -11,10 +12,11 @@ class Character:
         self.name = ""
         self.max_health = 0
 
+    # Method to roll 6 sided dice
     def roll_dice(self):
         return random.randint(1, 6)
 
-    # eventuell en generisk roll för allt
+    # Generic method for all roll mechanics (not used atm)
     def roll(self, value):
         sum = 0
         for i in range(value):
@@ -22,6 +24,7 @@ class Character:
         print(f"{self.name}: I try something! (Rolled {sum})")
         return sum
 
+    # Rolls a dice for each attack_value of character
     def attack_roll(self):
         attack = 0
         for i in range(self.attack):
@@ -36,10 +39,6 @@ class Character:
         print(f"{self.name}: I try initiate! ({initative} initative roll)")
         return initative
 
-    def death(self):
-        self.is_alive = False
-        print(f"{self.name}: im death gg")
-
     def dodge_roll(self):
         dodge = 0
         for i in range(self.agility):
@@ -47,10 +46,16 @@ class Character:
         print(f"{self.name}: I try dodge! ({dodge} dodge roll")
         return dodge
 
+    def death(self):
+        self.is_alive = False
+        print(f"{self.name}: im death gg")
+
+    # Method to take damage
     def take_dmg(self):
         self.health -= 1
         if self.health == 0:
             self.death()
 
+    # Method to heal monsters if player escapes or if player loads a character
     def heal(self):
         self.health = self.max_health
