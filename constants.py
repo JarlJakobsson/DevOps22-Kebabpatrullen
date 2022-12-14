29f32d6a218e26af49ffc5db62b928fb1976b66b
Alex Roman <alexandru.roman@yh.nackademin.se>
2022-12-14 02:03:24 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,28 +1,48 @@
-MAIN_MENU_TEXT = """
-###### START MENU #########
-[1] Create a new hero
-[2] Load hero
-[3] Quit
-############################
-"""
-NAME_TEXT = """
-                                ######### ENTER NAME ###########
-                                Plase enter your name
-                                ################################
 
-                                Enter your name: """
+MAIN_MENU_TEXT = """
+                                ####### START MENU #########
+                                [1] - Create a new hero
+                                [2] - Load hero
+                                [3] - Quit
+                                ############################
+                                Enter your choice: """
+
+
+NAME_TEXT = """
+                                ##### ENTER YOUR NAME ######
+                                # Please enter your name: """
+
+
 ROLE_TEXT = """
-####### CHOOSE ROLE ########
-[1] Knight
-[2] Wizard
-[3] Thief
-############################
-"""
+                                ##### CHOOSE CHARACTER ######
+                                [1] - Knight
+                                [2] - Wizard
+                                [3] - Thief
+                                #############################
+                                Enter your choice: """
+
+
 MOVE_MENU_TEXT = """
-########## MOVE ###########
-[1] - UP
-[2] - DOWN
-[3] - LEFT
-[4] - RIGHT
+########### MOVE ############
+[W] - UP
+[S] - DOWN
+[A] - LEFT
+[D] - RIGHT
+#############################
+
+Which way do you want to go? """
+
+
+EXIT_TEXT = """
+  YOU FOUND AN EXIT!
+[1] - Keep exploring
+[2] - Leave the dungeoun with all your treasure
+"""
+
+
+BATTLE_TEXT = """
+######### BATTLE ##########
+[1] - Attack
+[2] - Try Escape
 ###########################
 """
